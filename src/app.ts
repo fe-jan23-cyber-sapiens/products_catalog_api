7601e048ad244de9b438c752bf4e578b80dc669e
Lilia Obushenko <liliaobushenko@gmail.com>
2023-05-17 16:23:56 +0300

    file removed

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 /* eslint-disable no-console */
 'use strict';
 
-import { createServer } from './createServer';
-
 const PORT = process.env.PORT || 5000;
+
+import express from 'express';
+import cors from 'cors';
+
+import productsRouter from './routes/Products';
+import detailsRouter from './routes/Details';
+
+export const createServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(express.static('public'));
+  app.use('/products', express.json(), productsRouter);
+  app.use('/details', express.json(), detailsRouter);
+
+  return app;
+};
 
 createServer().listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
